09681217e0f13b461a2714c7a39f6625d30cf64e
Karolina Cwynar <karolinacwnr@gmail.com>
2022-02-01 17:57:15 +0100

    Added comments

--- Homework/homework2.js
+++ Homework/homework2.js
@@ -1,4 +1,4 @@
-zad.1 Funkcja solution1 zwraca index min elementu (Wartość absolutna)
+// zad.1 Funkcja solution1 zwraca index min elementu (Wartość absolutna)
 const arr = [6, 2, 10, 4, 2, -1, 3, 5]
 solution1(arr) // 5
 
@@ -21,7 +21,7 @@ solution1(arr);
 
 
 
-zad.2. Funkcja solution2 zwraca ilosc dodatnich, ujemnych oraz zer
+// zad.2. Funkcja solution2 zwraca ilosc dodatnich, ujemnych oraz zer
 
 function solution2(arr) {
     let positive = 0;
@@ -48,7 +48,7 @@ console.log(result);
 
 
 
-zad.3. Funkcja solution3 zwraca elementy ktore są wieksze niz średnia arytmetyczna.
+// zad.3. Funkcja solution3 zwraca elementy ktore są wieksze niz średnia arytmetyczna.
 
 
 function solution3(arr) {
@@ -74,7 +74,7 @@ const result = solution3(arr)     // [6, 10, 4, 5] w tym przypadku S.A jest 3.87
 console.log(result);
 
 
-zad.4.  Funkcja solution4 przyjmuje text. Zwraca najdluzsze zdanie
+// zad.4.  Funkcja solution4 przyjmuje text. Zwraca najdluzsze zdanie
 
 const text = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`
 //solution4(text) // 'Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
@@ -97,7 +97,7 @@ function solution4(text) {
 const result = solution4(text)
 console.log(result)
 
-zad.5.  Funkcja solution5 przyjmuje text, zwraca najdluzsze slowo 
+// zad.5.  Funkcja solution5 przyjmuje text, zwraca najdluzsze slowo 
 
 function solution5 (text) {
     let splitText = text.split(' ');
