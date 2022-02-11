d28f8397a064fafaa468edd7a9faf59358cfd659
Karolina Cwynar <karolinacwnr@gmail.com>
2022-02-11 10:52:02 +0100

    Changed function to class

--- Homework/homework4/homework4.js
+++ Homework/homework4/homework4.js
@@ -1,16 +1,24 @@
-function Product(name, price) {
-    return {
-        name,
-        price
-    }
+// function Product(name, price) {
+//     return {
+//         name,
+//         price
+//     }
+// }
+
+//Przepisac funkcje Product na klasę Product
+
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;      
+        }
 }
 
-
-// Stworzona TABLICA zawierająca 3 obiekty (instancje Person)
+// Stworzona TABLICA zawierająca 3 obiekty (instancje Person) - dodajemy new, aby tworzyć obiekty przez klasę
 const priceList = [
-    Product('Milk', 1),
-    Product('Bread', 2),
-    Product('Egg', 0.3)
+    new Product('Milk', 1),
+    new Product('Bread', 2),
+    new Product('Egg', 0.3)
 ];
 
 // "pojemnik" na ordery
@@ -21,7 +29,7 @@ const orderList = [];
 function renderPriceList() {
     const priceListElement = document.getElementById('priceList');
     priceListElement.innerHTML = '';
-    for(let i =0; i< priceList.length; i++) {
+    for (let i = 0; i < priceList.length; i++) {
         const productElement = document.createElement('li'); // DOCS => https://www.w3schools.com/jsref/met_document_createelement.asp
         productElement.innerText = `${priceList[i].name} cost ${priceList[i].price}`;  // priceList[i].name + " cost " + priceList[i].price
         priceListElement.appendChild(productElement); // DOCS => https://www.w3schools.com/jsref/met_node_appendchild.asp
@@ -33,19 +41,19 @@ function renderPriceList() {
 //Funkcja wypisuje (renderuje) listę zamówień
 
 function renderOrderList() {
-    const orderListElement =  document.getElementById('productsList');
+    const orderListElement = document.getElementById('productsList');
 
     orderListElement.innerHTML = '';
 
-    for(let i = 0; i< orderList.length; i++) {
+    for (let i = 0; i < orderList.length; i++) {
         const productElement = document.createElement('li');
         productElement.innerText = `${orderList[i].name} x ${orderList[i].amount}`;
         orderListElement.appendChild(productElement);
-    
-    //Zadanie domowe: jeśli cena * ilośc mieści się w budżecie, to pokoloruj na zielono, jeśli nie, to na czerwono
+
+        //Zadanie domowe: jeśli cena * ilośc mieści się w budżecie, to pokoloruj na zielono, jeśli nie, to na czerwono
         if (
-            orderList[i].amount * getPriceByName(orderList[i].name) <= orderList[i].budget 
-         ) { 
+            orderList[i].amount * getPriceByName(orderList[i].name) <= orderList[i].budget
+        ) {
             productElement.classList.add("green")
         } else {
             productElement.classList.add("red")
@@ -56,7 +64,7 @@ function renderOrderList() {
 //Dodanie funkcji pobierającej cenę z priceList
 
 function getPriceByName(name) {
-// dodac petle po calej liscie z cenami i poszukac nazwy produktu, jesli jest to wyciagnac cenei zwrocic
+    // dodac petle po calej liscie z cenami i poszukac nazwy produktu, jesli jest to wyciagnac cenei zwrocic
     for (let i = 0; i < priceList.length; i++) {
         if (name === priceList[i].name) {
             return priceList[i].price
@@ -72,12 +80,12 @@ function isValidData() {
     const productAmountElement = document.getElementById('productAmount');
     const productMaxPriceElement = document.getElementById('productMaxPrice');
 
-    if (productNameElement.value === '') {
+    if (productNameDropdown.value === '') {
         console.error('product name can not be empty');
         return false;
     }
-    if (productAmountElement.value === '' 
-    || Number.isNaN(Number(productAmountElement.value))) {
+    if (productAmountElement.value === ''
+        || Number.isNaN(Number(productAmountElement.value))) {
         console.error('product amount can not be empty');
         return false;
     }
@@ -91,12 +99,12 @@ function isValidData() {
 function init() {
     const myBtn = document.getElementById('myBtn');
 
-//Dodajemy nasłuchiwacza do buttona na click
+    //Dodajemy nasłuchiwacza do buttona na click
 
     myBtn.addEventListener('click', (event) => {
         event.preventDefault(); // Zapobiega domyślnemy zachowaniu przelądarki po kliknięciu w button, który jest w formularzu
-        
-        const productNameElement = document.getElementById('productName');
+
+        const productNameDropdown = document.getElementById("productNameDropdown"); //zamiast inputa deklaruję dropdown po ID
         const productAmountElement = document.getElementById('productAmount');
         const productMaxPriceElement = document.getElementById('productMaxPrice');
 
@@ -107,20 +115,39 @@ function init() {
         }
 
         const order = {
-            name: productNameElement.value, 
-            amount: productAmountElement.value, 
+            name: productNameDropdown.value, // zmieniam value z inputa na dropdown
+            amount: productAmountElement.value,
             budget: productMaxPriceElement.value
         }
 
         orderList.push(order);
         renderOrderList();
 
-        productNameElement.value = '';
+        //productNameDropdown.value = '';
         productAmountElement.value = '';
         productMaxPriceElement.value = '';
     });
 }
 
+
+//Homework 5
+
+// Funkcja ma przejść przez całą listę priceList, pobrać priceList[i].name i wyrenderować w dropdownie
+
+function renderNameOfProduct(name) {
+
+    const productNameDropdown = document.getElementById("productNameDropdown"); //deklaruje dropdown po ID 
+
+    for (let i = 0; i < priceList.length; i++) {
+
+        const productOption = document.createElement('option');
+        productOption.innerHTML = `${priceList[i].name}`;
+        productNameDropdown.appendChild(productOption);
+    }
+
+}
+
 init();
 renderPriceList();
 renderOrderList();
+renderNameOfProduct();
